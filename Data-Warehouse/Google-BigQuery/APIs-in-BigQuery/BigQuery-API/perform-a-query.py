99c257aedda017c7e26b8e5201f6cda988924362
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-03-21 20:44:04 +0300

    added a folder for various BigQuery APIs

--- Data-Warehouse/Google-BigQuery/APIs-in-BigQuery/BigQuery-API/perform-a-query.py
+++ Data-Warehouse/Google-BigQuery/APIs-in-BigQuery/BigQuery-API/perform-a-query.py
@@ -4,11 +4,11 @@ from google.cloud import bigquery
 # print(dir(bigquery))
 
 # Instantiate the bigquery.Client class to create the BigQuery client.
-client = bigquery.Client()
+client = bigquery.Client(project="xxxxxxxxxx") # insert the project id as a parameter
 
 # Perform a sample query
 QUERY = (
-    'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013`'
+    'SELECT * FROM `bigquery-public-data.usa_names.usa_1910_2013`'
     'WHERE state = "TX" '
     'LIMIT 100'
 )
@@ -17,4 +17,4 @@ query_job = client.query(QUERY) # API request
 rows = query_job.result() # Waits for query to finish
 
 for row in rows:
-    print(row.name)
+    print(row)
