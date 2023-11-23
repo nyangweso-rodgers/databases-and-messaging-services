34571ac55bf2a29e62b72b06307cc998211137b3
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-11-23 15:16:49 +0300

    modified sale_item.js schema

--- MongoDB/MongoDB-with-Node.js/mongodb-mongoose/model/sale_item.js
+++ MongoDB/MongoDB-with-Node.js/mongodb-mongoose/model/sale_item.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const saleItemSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     immutable: true,
@@ -14,8 +18,21 @@ const saleItemSchema = new Schema({
   },
   created_by_name: String,
   updated_by_name: String,
-  active: Boolean,
+  active: {
+    type: Boolean,
+    required: true,
+  },
+  item_code: {
+    type: String,
+    required: true,
+  },
+  item_group: String,
+  item_uom: {
+    type: String,
+    required: true,
+  },
+  description: String,
 });
 
-const sale_item = model("sale_item", saleInvoiceSchema);
-export default sale_item;
+const SaleItem = model("sale_item", saleInvoiceSchema);
+export default SaleItem;
