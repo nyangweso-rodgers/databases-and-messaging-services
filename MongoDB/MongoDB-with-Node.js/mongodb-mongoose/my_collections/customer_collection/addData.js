5c3e17fc2e8326bd776d9fd1f9ca021e759fa486
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-12-07 22:29:27 +0300

    added data.json and modified addData.js

--- MongoDB/MongoDB-with-Node.js/mongodb-mongoose/my_collections/customer_collection/addData.js
+++ MongoDB/MongoDB-with-Node.js/mongodb-mongoose/my_collections/customer_collection/addData.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
-//import customer from "../../model/customerData.js";
+import customer from "../../model/customerData.js";
 
-mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(
+  // enter connection string here to connect
+  process.env.MONGODB_URI
+);
 
-// check and Test Connection
-const db = mongoose.connection;
-db.on("error", (error) => console.error("Connection error:", error));
-db.once("open", () => console.log("Connected to MongoDB Atlas"));
+// Read data from the JSON file
