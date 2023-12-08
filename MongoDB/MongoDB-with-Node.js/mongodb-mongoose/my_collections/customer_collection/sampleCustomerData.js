d7cc61a780111c9b0f6333cc8139c56b4a9e251f
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-12-08 21:19:54 +0300

    file format

--- MongoDB/MongoDB-with-Node.js/mongodb-mongoose/my_collections/customer_collection/sampleCustomerData.js
+++ MongoDB/MongoDB-with-Node.js/mongodb-mongoose/my_collections/customer_collection/sampleCustomerData.js
@@ -1,20 +1,20 @@
 [
   {
-    "customer_id": "10",
-    "first_name": "Test Customer",
-    "last_name": "Test Customer",
-    "gender": "Male"
+    customer_id: "10",
+    first_name: "Test Customer",
+    last_name: "Test Customer",
+    gender: "Male",
   },
   {
-    "customer_id": "11",
-    "first_name": "Test Customer",
-    "last_name": "Test Customer",
-    "gender": "Male"
+    customer_id: "11",
+    first_name: "Test Customer",
+    last_name: "Test Customer",
+    gender: "Male",
   },
   {
-    "customer_id": "12",
-    "first_name": "Test Customer",
-    "last_name": "Test Customer",
-    "gender": "Male"
-  }
-]
+    customer_id: "12",
+    first_name: "Test Customer",
+    last_name: "Test Customer",
+    gender: "Male",
+  },
+];
