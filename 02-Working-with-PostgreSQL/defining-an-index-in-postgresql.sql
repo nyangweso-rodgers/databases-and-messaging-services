fffb1a9102c7cef2a7f365f50e5a925f8343a9e6
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-19 22:20:30 +0300

    deleted 2 sql files

--- 02-Working-with-PostgreSQL/defining-an-index-in-postgresql.sql
+++
@@ -1,5 +0,0 @@
--- PostgreSQL - defining an index
-      CREATE INDEX IDX_STUDENT_ROLL_NO
-      ON STUDENT(ROLL_NO);
-      -- Table = STUDENT
-      -- Column = ROLL_NO
--- 02-Working-with-PostgreSQL/show-tables-in-postgresql.sql
+++
@@ -1,5 +0,0 @@
--- show tables in PostgreSQL
-    select * 
-    from pg_catalog.pg_tables
-    where schemaname != 'pg_catalog' and schemaname != 'information_schema'
-    -- we used a condition in the WHERE clause to filter system tables. If you omit the WHERE clause, you will get many tables including the system tables.
