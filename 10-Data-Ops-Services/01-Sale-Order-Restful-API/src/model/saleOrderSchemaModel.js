0e688a044169429bc5fa87a6a960f00cce67c023
nyangweso-rodgers <rodgerso65@gmail.com>
2024-03-14 07:49:43 +0300

    added code to the sale-order schema

--- 10-Data-Ops-Services/01-Sale-Order-Restful-API/src/model/saleOrderSchemaModel.js
+++ 10-Data-Ops-Services/01-Sale-Order-Restful-API/src/model/saleOrderSchemaModel.js
@@ -3,6 +3,23 @@ const { Schema, model } = mongoose;
 
 const saleOrderSchema = new Schema(
   {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+        const day = String(currentDate.getDate()).padStart(2, "0");
+        const hours = String(currentDate.getHours()).padStart(2, "0");
+        const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+        const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+
+        return `SO-${year}${month}${day}${hours}${minutes}${seconds}`;
+      },
+      immutable: true,
+    },
     customer: {
       type: "String",
       required: true,
