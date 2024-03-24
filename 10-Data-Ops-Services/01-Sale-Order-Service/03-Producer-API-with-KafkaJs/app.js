437e47464dbbbe773e53e3c302f46172a0be196a
nyangweso-rodgers <rodgerso65@gmail.com>
2024-03-24 21:32:35 +0300

    added package.* files

--- 10-Data-Ops-Services/01-Sale-Order-Service/03-Producer-API-with-KafkaJs/app.js
+++ 10-Data-Ops-Services/01-Sale-Order-Service/03-Producer-API-with-KafkaJs/app.js
@@ -1,13 +1,22 @@
 //Producer API with KafkaJs
 
 import express from "express";
+import "dotenv/config";
 
 // create an express application object
 const app = express();
+const port = process.env.PORT;
 
+//Middleware
+app.use(express.json());
+
+// Routes will be written here
 
 // start the server
-const port = 80;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, (error) => {
+  if (!error) {
+    console.log(`Server is running on http://localhost:${port}`);
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
 });
