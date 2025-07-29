b64b255b1197909c86621d90a9ef54ec3c8341aa
nyangweso-rodgers <rodgerso65@gmail.com>
2025-07-29 07:43:09 +0300

    posgtres information schema sql scripts

---
+++ 01-databases/01-postgres/02-commands/01-sql/information-schema/01-list-db-tables.sql
@@ -0,0 +1,5 @@
+SELECT table_schema, table_name
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE'
+  AND table_schema NOT IN ('pg_catalog', 'information_schema')
+ORDER BY table_schema, table_name;
